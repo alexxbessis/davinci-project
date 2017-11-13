5853a891c2aea85eb95bbb5f72b2d053ea42d6a1
CDamasse <charles.damasse@gmail.com>
2017-11-13 18:53:57 +0100

    accueil

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ require_once 'issam.php';
     <meta charset="UTF-8">
     <title>Title</title>
     <link rel="stylesheet" href="css/style.css">
-    <title>AKEX</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 <body>
@@ -22,13 +21,56 @@ require_once 'issam.php';
     </div>
     <div class="row">
         <div class="row">
+                <div class="btn btn-secondary col-6">
+                    <strong>Identifiant</strong>
+                    <p>...</p>
+                    <strong>Num.badge</strong>
+                    <p>...</p>
+                    <strong>ID Administratif</strong>
+                    <p>...</p>
+                </div>
+                <div class="btn btn-secondary col-6">
 
+                </div>
+                <div class="col">
+                    <img src="" alt="">
+                    <a href="#" class="btn btn-secondary btn-sm">Changer</a>
+                </div>
+            <!--
+            <div class="row">
+                <h6>Inscriptions aux programmes</h6>
+                <table class="table">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th>Programme</th>
+                        <th>Année dans le cursus</th>
+                        <th>Année</th>
+                        <th>Délégué</th>
+                        <th>Date cloture</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            -->
         </div>
         <div class="row">
 
-        </div>
-        <div class="row">
-            
         </div>
     </div>
 </div>
