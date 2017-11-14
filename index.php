d7443206a89564af472c943b5afa5efbf8d048c0
CDamasse <charles.damasse@gmail.com>
2017-11-14 13:53:48 +0100

    up

--- index.php
+++ index.php
@@ -8,18 +8,20 @@ require_once 'issam.php';
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/50c50d149f.js"></script>
 </head>
 <body>
 
 <div class="container">
     <div class="row">
-        <div class="col-12" id="header_fiche">
+        <div class="col-12 header_fiche">
             <h3>Fiche Etudiant : </h3>
         </div>
     </div>
     <div class="row justify-content-center" id="grande_row">
-        <div class="row col-6">
+        <div class="row-fluid col-6">
             <div class="row col-12">
                 <div class="col-4">
                     <div class="card">
@@ -54,44 +56,52 @@ require_once 'issam.php';
                     <a href="#" class="btn btn-secondary btn-small offset-2">Changer</a>
                 </div>
             </div>
-            <div class="row justify-content-center col-12" id="tableau">
+            <div class=" col-12" id="tableau">
                 <table class="table">
                     <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Username</th>
+                        <th>Programme</th>
+                        <th>Année dans le cursus</th>
+                        <th>Année</th>
+                        <th>Délégué</th>
+                        <th>Date cloture</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <th scope="row">PAIIMWB2</th>
+                        <td>ANNEE 2 - AXE DEVELOPPEMENT WEB</td>
+                        <td>2017</td>
+                        <td></td>
+                        <td></td>
                     </tr>
                     <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
+                        <th scope="row">PAIIMNS1</th>
+                        <td>ANNEE PREPARATOIRE</td>
+                        <td>2016</td>
+                        <td></td>
+                        <td></td>
                     </tr>
                     </tbody>
                 </table>
             </div>
+            <div class="row-fluid">
+                <div class="header_fiche col-12">
+                   <h5> Mes groupes</h5>
+                </div>
+                <div class="row">
+
+                </div>
+            </div>
         </div>
-        <div class="row col-6 no-gutters">
+        <div class="row col-6 no-gutters" id="row_droite">
 
         </div>
     </div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </body>
 </html>
