6a6f2babe61881cfac2dcdc9aa359237a2f53b18
Alexx Bessis <alex.bessis@gmail.com>
2017-11-15 16:25:02 +0100

    pulv

--- absence.php
+++ absence.php
@@ -6,7 +6,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Portail PULV</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/50c50d149f.js"></script>
 </head>
